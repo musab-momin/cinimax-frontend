ae2c184338607fc0c4ba0f121a8b25ebd0d8c310
Musab Momin <mominmusab127@gmail.com>
2024-05-17 16:35:42 +0530

    feat: navbar completed

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import cx from "classnames";
+import classes from "./navbar.module.scss";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <header></header>;
+  return (
+    <header className={classes.header}>
+      <nav className="container flex-bx align-center justify-between">
+        <h2 className={classes.logo}>Cinimax</h2>
+        <ul className="flex-bx gap2">
+          <li>
+            <Link href={"/login"}>Login</Link>
+          </li>
+          <li>
+            <Link href={"/signup"} className={classes.startedBtn}>
+              Get Started
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
